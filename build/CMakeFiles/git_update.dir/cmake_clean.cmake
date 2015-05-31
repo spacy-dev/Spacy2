a1b706c85700c9e5e216a9b1e9a8a58858aac3cc
Lars Lubkoll <lars.lubkoll@posteo.de>
2015-05-31 16:42:47 +0200

    removed build-dir

--- build/CMakeFiles/git_update.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/git_update"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/git_update.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
