2b87b3df8e665574cefc3ff348602403f0e34de7
Anton Schiela <bt303011@btmlx1.mat.uni-bayxreuth.de>
2016-01-21 09:04:42 +0100

    Cover treatment of Product-Space Vectors with Eigen

---
+++ Spacy/Adapter/Eigen/util.hh
@@ -0,0 +1,28 @@
+// Copyright (C) 2015 by Lars Lubkoll. All rights reserved.
+// Released under the terms of the GNU General Public License version 3 or later.
+
+#ifndef SPACY_ADAPTER_EIGEN_UTIL_HH
+#define SPACY_ADAPTER_EIGEN_UTIL_HH
+
+#include "Eigen/Dense"
+#include "Spacy/vector.hh"
+
+namespace Spacy
+{
+  namespace Rn
+  {
+    /**
+     * @ingroup EigenGroup, VectorSpaceGroup
+     * @brief %Vector for %Rn, based on the %Eigen library.
+     */
+    ///  Copy ::Spacy::Vector to flat coefficient vector of %Eigen .
+    void copy(const ::Spacy::Vector& x, Eigen::VectorXd& y);
+ 
+    ///  Copy flat coefficient vector of %Eigen to ::Spacy::Vector.
+    void copy(const Eigen::VectorXd& x, ::Spacy::Vector& y);
+    
+    unsigned getSize(const ::Spacy::Vector& y);
+  }
+}
+
+#endif // SPACY_ADAPTER_EIGEN_UTIL_HH
