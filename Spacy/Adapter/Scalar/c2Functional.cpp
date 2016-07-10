2e4aaec59780c124bdb1fae678686bdefff2ba82
Lars Lubkoll <lars.lubkoll@posteo.de>
2016-07-10 15:36:09 +0200

    Refactor and fix missing include

---
+++ Spacy/Adapter/Scalar/c2Functional.cpp
@@ -0,0 +1,40 @@
+#include "c2Functional.hh"
+
+#include "Spacy/linearOperator.hh"
+#include "Spacy/vector.hh"
+#include "Spacy/Spaces/realSpace.hh"
+#include "Spacy/Util/cast.hh"
+
+namespace Spacy
+{
+    namespace Scalar
+    {
+        C2Functional::C2Functional(std::function<double(double)> value,
+                                   std::function<double(double)> derivative,
+                                   std::function<double(double)> secDerivative) :
+            Spacy::FunctionalBase(Space::R) ,
+            value_(std::move(value)),
+            derivative_(std::move(derivative)),
+            secDerivative_(std::move(secDerivative))
+        {}
+
+        Spacy::Real C2Functional::operator()(const ::Spacy::Vector& x) const
+        {
+            return value_(cast_ref<Spacy::Real>(x));
+        }
+
+        Spacy::Vector C2Functional::d1(const ::Spacy::Vector& x) const
+        {
+            return Spacy::Real(derivative_(cast_ref<Spacy::Real>(x)));
+        }
+
+        Spacy::Vector C2Functional::d2(const ::Spacy::Vector& x, const ::Spacy::Vector& dx) const
+        {
+            return Spacy::Real( secDerivative_( cast_ref<Spacy::Real>(x))  * cast_ref<Spacy::Real>(dx) );
+        }
+        Spacy::Scalar::LinearOperator C2Functional::hessian(const ::Spacy::Vector& x) const
+        {
+            return Spacy::Scalar::LinearOperator( *operatorSpace_, secDerivative_( cast_ref<Spacy::Real>(x) ));
+        }
+    }
+}
--- Spacy/Adapter/Scalar/c2Functional.hh
+++ Spacy/Adapter/Scalar/c2Functional.hh
@@ -1,5 +1,4 @@
-#ifndef SPACY_ADAPTER_SCALAR_C2_FUNCTIONAL_HH
-#define SPACY_ADAPTER_SCALAR_C2_FUNCTIONAL_HH
+#pragma once
 
 #include <functional>
 #include <memory>
@@ -9,64 +8,44 @@
 
 namespace Spacy
 {
-  /// @cond
-  class LinearOperator;
-  class Real;
-  class Vector;
-  /// @endcond
-
-  /** @addtogroup ScalarGroup @{ */
-namespace Scalar
-{
     /// @cond
     class LinearOperator;
+    class Real;
+    class Vector;
     /// @endcond
 
-class C2Functional : public Spacy::FunctionalBase 
-{
-public:
-  C2Functional(std::function<double(double)> value,
-               std::function<double(double)> derivative, 
-               std::function<double(double)> secDerivative) :
-               Spacy::FunctionalBase(Space::R) ,
-               value_(value), 
-               derivative_(derivative), 
-               secDerivative_(secDerivative) {};
-  // Compute f(x).
-  Spacy::Real operator()(const ::Spacy::Vector& x) const
- {
-   return value_(cast_ref<Spacy::Real>(x));
- }
- 
-  // Compute f'(x) as element of X*.
-  Spacy::Vector d1(const ::Spacy::Vector& x) const
-  {
-   return Spacy::Real(derivative_(cast_ref<Spacy::Real>(x)));
-  }
-  
-      // Compute f''(x)dx as element of X*.
-  Spacy::Vector d2(const ::Spacy::Vector& x, const ::Spacy::Vector& dx) const
-  {
-	  return Spacy::Real( secDerivative_( cast_ref<Spacy::Real>(x))  * cast_ref<Spacy::Real>(dx) );
-  }
-  // Access f''(x) as mapping f''(x): X->X*.
-  Spacy::Scalar::LinearOperator hessian(const ::Spacy::Vector& x) const
-  {
-   return Spacy::Scalar::LinearOperator( *operatorSpace_, secDerivative_( cast_ref<Spacy::Real>(x) ));
-  }
- 
-private:
-  std::shared_ptr<VectorSpace> operatorSpace_ = nullptr;
-  std::function<double(const double&)> value_ ;
-  std::function<double(const double&)> derivative_;
-  std::function<double(const double&)> secDerivative_;
-  
-  
-};
+    /** @addtogroup ScalarGroup @{ */
+    namespace Scalar
+    {
+        /// @cond
+        class LinearOperator;
+        /// @endcond
 
+        class C2Functional : public Spacy::FunctionalBase
+        {
+        public:
+            C2Functional(std::function<double(double)> value,
+                         std::function<double(double)> derivative,
+                         std::function<double(double)> secDerivative);
 
+            // Compute f(x).
+            Spacy::Real operator()(const ::Spacy::Vector& x) const;
+
+            // Compute f'(x) as element of X*.
+            Spacy::Vector d1(const ::Spacy::Vector& x) const;
+
+            // Compute f''(x)dx as element of X*.
+            Spacy::Vector d2(const ::Spacy::Vector& x, const ::Spacy::Vector& dx) const;
+
+            // Access f''(x) as mapping f''(x): X->X*.
+            Spacy::Scalar::LinearOperator hessian(const ::Spacy::Vector& x) const;
+
+        private:
+            std::shared_ptr<VectorSpace> operatorSpace_ = nullptr;
+            std::function<double(const double&)> value_ ;
+            std::function<double(const double&)> derivative_;
+            std::function<double(const double&)> secDerivative_;
+        };
+    }
+    /** @} */
 }
-  /** @} */
-}
-
-#endif // SPACY_ADAPTER_SCALAR_C2_FUNCTIONAL_HH
