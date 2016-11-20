9df708c535028303b1f1558e23baa17e5399d06d
Lars Lubkoll <lars.lubkoll@posteo.de>
2016-11-20 12:29:35 +0100

    [TRAVIS] Deploy doc

--- deploy_doc.sh
+++ deploy_doc.sh
@@ -30,37 +30,38 @@ echo "checkout $TARGET_BRANCH"
 git checkout $TARGET_BRANCH
 echo "remove doc"
 rm -rf doc
+cd ..
 
 # Run our compile script
-doCompile
+#doCompile
 
 # Now let's go have some fun with the cloned repo
-mv doc ../SpacyDoc/
-cd ../SpacyDoc
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
+#mv doc ../SpacyDoc/
+#cd ../SpacyDoc
+#git config user.name "Travis CI"
+#git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
+#if [ -z `git diff --exit-code` ]; then
+#    echo "No changes to the output on this push; exiting."
+#    exit 0
+#fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
-git add .
-git commit -m "Deploy to GitHub Pages: ${SHA}"
+#git add .
+#git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
+#ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+#ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+#ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+#ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+#openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+#chmod 600 deploy_key
+#eval `ssh-agent -s`
+#ssh-add deploy_key
 
 # Now that we're all set up, we can push.
-echo "pushing to $TARGET_BRANCH"
-git push $SSH_REPO $TARGET_BRANCH
+#echo "pushing to $TARGET_BRANCH"
+#git push $SSH_REPO $TARGET_BRANCH
