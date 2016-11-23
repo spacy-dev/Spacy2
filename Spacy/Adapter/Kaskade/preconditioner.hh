12c9e8b7abd546dfca19b3e6ad6b047dedc09845
mastunibayreuth <matthias.stoecklein@uni-bayreuth.de>
2016-11-23 14:54:54 +0100

    Wrapper Class to insert a Kaskade preconditioner into the Spacy framework

--- Spacy/Adapter/Kaskade/preconditioner.hh
+++ Spacy/Adapter/Kaskade/preconditioner.hh
@@ -1,8 +1,7 @@
 // Copyright (C) 2015 by Lars Lubkoll. All rights reserved.
 // Released under the terms of the GNU General Public License version 3 or later.
 
-#ifndef SPACY_ADAPTER_KASKADE_PRECONDITIONER_HH
-#define SPACY_ADAPTER_KASKADE_PRECONDITIONER_HH
+#pragma once
 
 #include "linalg/direct.hh"
 #include <Spacy/zeroVectorCreator.hh>
@@ -17,6 +16,14 @@ namespace Spacy
   namespace Kaskade
   {
       
+    /**
+     * @ingroup KaskadeGroup
+     * @brief Wrapper class to call apply-routines from different Kaskade preconditioners the same way (is to be unified in Kaskade)
+     * @param Prec  %Kaskade Preconditioner (i.e., %Kaskade::AdditiveMultiGrid < > )
+     * @param range range space of the preconditioner
+     * @param domain domain space of the preconditioner
+     */
+      
  template  <  class Range,  class Domain, class Prec>
  class Apply
  {
@@ -27,10 +34,14 @@ namespace Spacy
          prec.apply(y,x);
      }
      
-     
-     
  };
- 
+  /**
+     * @ingroup KaskadeGroup
+     * @brief Wrapper class to call apply-routines from different Kaskade preconditioners the same way (is to be unified in Kaskade)
+     * @param Prec  %Kaskade Preconditioner (i.e., %Kaskade::AdditiveMultiGrid < > )
+     * @param range range space of the preconditioner
+     * @param domain domain space of the preconditioner
+     */
  template <class Range,  class Domain,  class T,  class U,  class V>
  class Apply< Range,  Domain, ::Kaskade::AdditiveMultiGrid<T, U, V> >
  {
@@ -43,9 +54,9 @@ namespace Spacy
  };
     /**
      * @ingroup KaskadeGroup
-     * @brief Direct solver interface for %Kaskade 7.
+     * @brief Preconditioner interface for %Kaskade 7.
      */
-    template <class KaskadeOperator, class AnsatzVariableDescription, class TestVariableDescription, class Prec>
+    template < class AnsatzVariableDescription, class TestVariableDescription, class Prec>
     class Preconditioner :
         public OperatorBase
     {
@@ -56,17 +67,13 @@ namespace Spacy
       Preconditioner() = delete;
       /**
        * @brief Constructor.
-       * @param A %Kaskade operator (i.e., %AssembledGalerkinOperator or %MatrixRepresentedOperator)
        * @param spaces boost fusion forward sequence of space pointers required to initialize temporary storage
        * @param domain domain space of the solver
        * @param range range space of the solver
-       * @param directSolver solver type (DirectType::MUMPS (default), DirectType::UMFPACK, DirectType::UMFPACK3264 or DirectType::SUPERLU)
-       * @param property matrix property (MatrixProperties::GENERAL (default) or MatrixProperties::SYMMETRIC)
        */
-      Preconditioner(const KaskadeOperator& A, const VectorSpace& domain, const VectorSpace& range, Prec& prec)
+      Preconditioner( const VectorSpace& domain, const VectorSpace& range, Prec& prec)
         : OperatorBase(domain,range),
           prec_(prec),
-          A_(A),
           spaces_( extractSpaces<AnsatzVariableDescription>(domain) )
       {}
 
@@ -79,19 +86,9 @@ namespace Spacy
         Domain x_(AnsatzVariableDescription::template CoefficientVectorRepresentation<>::init(spaces_));
         copyToCoefficientVector<AnsatzVariableDescription>(x,x_);
 
-//         static const bool check_type_equal = std::is_same<Prec, KaskadeOperator>::value;
-        
-        // Note: apply arguments in correct order !!!!!!!!!!!!
-        
-       
-//         prec_.apply( ::Kaskade::component<0>(y_),  ::Kaskade::component<0>(x_));
-//         else
-//          prec_.apply(y_,x_);
-        
-
         Apply<Range, Domain, Prec>::wrap(prec_, y_, x_);
-//         auto y = ::Spacy
-         auto y = ::Spacy::zero(range());
+
+        auto y = ::Spacy::zero(range());
         copyFromCoefficientVector<TestVariableDescription>(y_,y);
 
         return y;
@@ -99,29 +96,25 @@ namespace Spacy
 
     private:
       Prec&  prec_;
-      const KaskadeOperator& A_;
       Spaces spaces_;
       
     };
 
     /**
-     * @brief Convenient generation of direct solver for %Kaskade 7.
+     * @brief Convenient generation of a Spacy preconditioner from a Kaskade preconditioner
      * @param A %Kaskade operator (i.e., AssembledGalerkinOperator or MatrixRepresentedOperator)
      * @param spaces boost fusion forward sequence of space pointers required to initialize temporary storage
      * @param domain domain space of the solver
      * @param range range space of the solver
-     * @param directSolver solver type (DirectType::MUMPS (default), DirectType::UMFPACK, DirectType::UMFPACK3264 or DirectType::SUPERLU)
-     * @param property matrix property (MatrixProperties::GENERAL (default) or MatrixProperties::SYMMETRIC)
-     * @return Preconditioner<KaskadeOperator,AnsatzVariableSetDescription,TestVariableSetDescription>( A , spaces , domain , range , directSolver , property )
+     * @param Prec Kaskade preconditioner (e.g. Kaskade::AdditiveMultiGrid < > )
+     * @return Preconditioner<AnsatzVariableSetDescription, TestVariableSetDescription, Preconditioner  >( domain , range , preconditioner )
      */
-    template <class AnsatzVariableSetDescription, class TestVariableSetDescription, class KaskadeOperator, class Prec>
-    auto makePreconditioner(const KaskadeOperator& A, const VectorSpace& domain, const VectorSpace& range, Prec&  prec)
+    template <class AnsatzVariableSetDescription, class TestVariableSetDescription, class Prec>
+    auto makePreconditioner(const VectorSpace& domain, const VectorSpace& range, Prec&  prec)
     {
-      return Preconditioner<KaskadeOperator,AnsatzVariableSetDescription,TestVariableSetDescription, Prec>
-          (  A, domain, range, prec);
+      return Preconditioner<AnsatzVariableSetDescription,TestVariableSetDescription, Prec>
+          ( domain, range, prec);
     }
 
   }
 }
-
-#endif // SPACY_ADAPTER_KASKADE_PRECONDITIONER_HH
