d0f8aae310d71f48d9e59c138cfeae4da886bbe1
Lars Lubkoll <lars.lubkoll@posteo.de>
2016-11-25 23:02:50 +0100

    Examples/FEniCS/PDE/Nonlinear_Heat_Transfer_PenaltyBC: Add script and instructions to run in example with docker

---
+++ Examples/FEniCS/PDE/Nonlinear_Heat_Transfer_PenaltyBC/compile_and_run.sh
@@ -0,0 +1,14 @@
+# Install Spacy
+git clone https://github.com/spacy-dev/Spacy.git
+cd Spacy && mkdir -p build && cd build && cmake .. -DDolfin=ON && make -j2 && sudo make install
+cd ../..
+
+# Generate problem definition from .ufl file
+ffc -l dolfin NonlinearHeat.ufl
+
+# Compilation:
+mkdir -p build && cd build && cmake .. && make
+ 
+# Run example:
+./run
+cd ..
