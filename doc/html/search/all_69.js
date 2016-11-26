8a4676332e0393fb20c6228b36a27104a5f978b8
Travis CI <lars.lubkoll@posteo.de>
2016-11-26 12:47:26 +0000

    Update documentation for commit: debe5cbb341a4e509cd95d760060a8b4bc725383

--- doc/html/search/all_69.js
+++ doc/html/search/all_69.js
@@ -11,15 +11,16 @@ var searchData=
   ['inducedscalarproduct',['InducedScalarProduct',['../classSpacy_1_1InducedScalarProduct.html',1,'Spacy']]],
   ['inducedscalarproduct',['InducedScalarProduct',['../classSpacy_1_1InducedScalarProduct.html#a8cf4d6c0823b6c9d59be783d3627130e',1,'Spacy::InducedScalarProduct']]],
   ['init',['init',['../classSpacy_1_1Generic_1_1Step.html#a91469ef55abf1cb43971a91c50edc0e5',1,'Spacy::Generic::Step']]],
-  ['invalidargument',['InvalidArgument',['../classSpacy_1_1Exception_1_1InvalidArgument.html#ae82799e1978a37928a446d1f316d071b',1,'Spacy::Exception::InvalidArgument']]],
   ['invalidargument',['InvalidArgument',['../classSpacy_1_1Exception_1_1InvalidArgument.html',1,'Spacy::Exception']]],
+  ['invalidargument',['InvalidArgument',['../classSpacy_1_1Exception_1_1InvalidArgument.html#ae82799e1978a37928a446d1f316d071b',1,'Spacy::Exception::InvalidArgument']]],
   ['inversedofmap',['inverseDofmap',['../classSpacy_1_1FEniCS_1_1VectorCreator.html#a8e369dcbb722462ee0bca008eae6b2f3',1,'Spacy::FEniCS::VectorCreator']]],
+  ['inverseidmap',['inverseIdMap',['../classSpacy_1_1ProductSpace_1_1VectorCreator.html#abfd2a000f770f0511dc864c43233a534',1,'Spacy::ProductSpace::VectorCreator']]],
   ['is',['is',['../namespaceSpacy.html#ae44bfd08fa77272bab4149665b26233a',1,'Spacy']]],
   ['isadmissible',['isAdmissible',['../classSpacy_1_1VectorSpace.html#a4beac78b082a364eb97e938b6c4928d2',1,'Spacy::VectorSpace']]],
   ['ishilbertspace',['isHilbertSpace',['../classSpacy_1_1VectorSpace.html#a12b023eff0cce202e4a5f9a754cc05c9',1,'Spacy::VectorSpace']]],
   ['ispositivedefinite',['isPositiveDefinite',['../classSpacy_1_1CG_1_1LinearSolver.html#adec5404c595dc514a9c81c9ccc06cf41',1,'Spacy::CG::LinearSolver']]],
   ['isprimalwrt',['isPrimalWRT',['../classSpacy_1_1VectorSpace.html#aae96ebd6f5e4a1a95ab8684b49062e7d',1,'Spacy::VectorSpace']]],
   ['isvoid',['IsVoid',['../structSpacy_1_1IsVoid.html',1,'Spacy']]],
-  ['iterativemethod',['IterativeMethod',['../classSpacy_1_1Generic_1_1IterativeMethod.html',1,'Spacy::Generic']]],
-  ['iterativemethod',['IterativeMethod',['../classSpacy_1_1Generic_1_1IterativeMethod.html#a0bb1df2e0e4bee756a2f3e3032743a1a',1,'Spacy::Generic::IterativeMethod']]]
+  ['iterativemethod',['IterativeMethod',['../classSpacy_1_1Generic_1_1IterativeMethod.html#a0bb1df2e0e4bee756a2f3e3032743a1a',1,'Spacy::Generic::IterativeMethod']]],
+  ['iterativemethod',['IterativeMethod',['../classSpacy_1_1Generic_1_1IterativeMethod.html',1,'Spacy::Generic']]]
 ];
--- doc/html/search/functions_69.js
+++ doc/html/search/functions_69.js
@@ -9,6 +9,7 @@ var searchData=
   ['init',['init',['../classSpacy_1_1Generic_1_1Step.html#a91469ef55abf1cb43971a91c50edc0e5',1,'Spacy::Generic::Step']]],
   ['invalidargument',['InvalidArgument',['../classSpacy_1_1Exception_1_1InvalidArgument.html#ae82799e1978a37928a446d1f316d071b',1,'Spacy::Exception::InvalidArgument']]],
   ['inversedofmap',['inverseDofmap',['../classSpacy_1_1FEniCS_1_1VectorCreator.html#a8e369dcbb722462ee0bca008eae6b2f3',1,'Spacy::FEniCS::VectorCreator']]],
+  ['inverseidmap',['inverseIdMap',['../classSpacy_1_1ProductSpace_1_1VectorCreator.html#abfd2a000f770f0511dc864c43233a534',1,'Spacy::ProductSpace::VectorCreator']]],
   ['is',['is',['../namespaceSpacy.html#ae44bfd08fa77272bab4149665b26233a',1,'Spacy']]],
   ['isadmissible',['isAdmissible',['../classSpacy_1_1VectorSpace.html#a4beac78b082a364eb97e938b6c4928d2',1,'Spacy::VectorSpace']]],
   ['ishilbertspace',['isHilbertSpace',['../classSpacy_1_1VectorSpace.html#a12b023eff0cce202e4a5f9a754cc05c9',1,'Spacy::VectorSpace']]],
