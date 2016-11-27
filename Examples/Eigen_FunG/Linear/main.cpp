4e1bbea3588133ab9959f5f3a8f3a5428128f1d7
Lars Lubkoll <lars.lubkoll@posteo.de>
2016-11-27 12:22:30 +0100

    Add linear and nonlinear example using FunG and Eigen

--- Examples/Eigen_FunG/Linear/main.cpp
+++ Examples/Eigen_FunG/Linear/main.cpp
@@ -1,9 +1,9 @@
-#include <tuple>
-
 #include <Eigen/Dense>
 #include <Spacy/spacy.h>
 #include <Spacy/Adapter/eigen.hh>
 #include <fung/fung.hh>
+
+#include <iostream>
 
 const constexpr int dim = 2;
 using Vector = Eigen::VectorXd;
@@ -11,18 +11,18 @@ using Matrix = Eigen::MatrixXd;
 
 auto get_fung_operator()
 {
-    Matrix A_(dim,dim);
-    A_.fill(0);
-    A_(0,0) = 1;
-    A_(1,1) = 2;
+    Matrix A(dim,dim);
+    A.fill(0);
+    A(0,0) = 1;
+    A(1,1) = 2;
 
-    Vector v_(dim), b(dim);
-    v_.fill(0);
-    b(0) = -1;
-    b(1) = -16;
+    Vector b(dim);
+    b(0) = -10;
+    b(1) = -20;
 
-    auto v = FunG::variable<0>(v_);
-    return A_*v + b;
+    // use FunG to generate operator
+    auto v = FunG::variable<0>(Vector(dim));
+    return A*v + b;
 }
 
 int main()
@@ -33,6 +33,7 @@ int main()
     auto A_ = get_fung_operator();
     auto V = Rn::makeHilbertSpace(dim);
     auto A = Rn::getFunGC1Operator(A_, V, V.dualSpace());
+    V.setScalarProduct( InducedScalarProduct( A.linearization(zero(V)) ) );
 
     auto x = localNewton(A);
     std::cout << "\nsolution:\n" << get(cast_ref<Rn::Vector>(x)) << '\n' << std::endl;
---
+++ Examples/Eigen_FunG/Nonlinear/main.cpp
@@ -0,0 +1,40 @@
+#include <Eigen/Dense>
+#include <Spacy/spacy.h>
+#include <Spacy/Adapter/eigen.hh>
+#include <fung/fung.hh>
+
+#include <iostream>
+
+const constexpr int dim = 2;
+using Vector = Eigen::VectorXd;
+using Matrix = Eigen::MatrixXd;
+
+auto get_fung_operator()
+{
+    Matrix A(dim,dim);
+    A.fill(0);
+    A(0,0) = 1;
+    A(1,1) = 2;
+
+    Vector b(dim);
+    b(0) = -10;
+    b(1) = -20;
+
+    // use FunG to generate operator
+    auto v = FunG::variable<0>(Vector(dim));
+    return (1+dot(v,A*v))*v + b;
+}
+
+int main()
+{
+    std::cout << std::endl;
+    using namespace Spacy;
+
+    auto A_ = get_fung_operator();
+    auto V = Rn::makeHilbertSpace(dim);
+    auto A = Rn::getFunGC1Operator(A_, V, V.dualSpace());
+    V.setScalarProduct( InducedScalarProduct( A.linearization(zero(V)) ) );
+
+    auto x = localNewton(A);
+    std::cout << "\nsolution:\n" << get(cast_ref<Rn::Vector>(x)) << '\n' << std::endl;
+}
