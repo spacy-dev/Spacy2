621ed77ef60e17d2202e7ff1804f4167f14325ca
Travis CI <lars.lubkoll@posteo.de>
2016-12-18 21:49:57 +0000

    Update documentation for commit: 11183a25b0f5f36043f287f6467fb328cf1c2883

--- doc/html/search/all_65.js
+++ doc/html/search/all_65.js
@@ -3,5 +3,6 @@ var searchData=
   ['eps',['Eps',['../classSpacy_1_1Mixin_1_1Eps.html',1,'Spacy::Mixin']]],
   ['eps',['eps',['../classSpacy_1_1Mixin_1_1Eps.html#a812b99b0abc1d78a34b4114907f23f52',1,'Spacy::Mixin::Eps::eps() const noexcept'],['../classSpacy_1_1Mixin_1_1Eps.html#af616ae8e55a645cefd4d2d4504d6705a',1,'Spacy::Mixin::Eps::Eps(Real eps=1e-15) noexcept']]],
   ['euclideanscalarproduct',['EuclideanScalarProduct',['../classSpacy_1_1Generic_1_1EuclideanScalarProduct.html',1,'Spacy::Generic']]],
-  ['extractspaces',['extractSpaces',['../namespaceSpacy_1_1Kaskade.html#afd93d7104ab12488e127eada5d714759',1,'Spacy::Kaskade']]]
+  ['extractspaces',['extractSpaces',['../namespaceSpacy_1_1Kaskade.html#afd93d7104ab12488e127eada5d714759',1,'Spacy::Kaskade']]],
+  ['extractsubspace',['extractSubSpace',['../group__ProductSpaceGroup.html#ga6054e5c78652ac4959f5d521acadd86a',1,'Spacy']]]
 ];
