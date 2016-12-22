a1cb61f1bcb333059fa83ebd81ed720ce4aad650
Lars Lubkoll <lars.lubkoll@posteo.de>
2016-12-22 23:00:22 +0100

    Adjust mock to refactoring
    Change-Id: Ie2bce9bab1e3493f46f4b7d53289d38635d47342

--- Test/Mock/linearOperator.hh
+++ Test/Mock/linearOperator.hh
@@ -3,9 +3,9 @@
 #include <Spacy/linearSolver.hh>
 #include <Spacy/zeroVectorCreator.hh>
 #include <Spacy/Util/Mixins/get.hh>
-#include <Spacy/Util/Base/addArithmeticOperators.hh>
-#include <Spacy/Util/Base/operatorBase.hh>
-#include <Spacy/Util/Base/vectorBase.hh>
+#include <Spacy/Util/Base/AddArithmeticOperators.hh>
+#include <Spacy/Util/Base/OperatorBase.hh>
+#include <Spacy/Util/Base/VectorBase.hh>
 
 namespace Mock
 {
