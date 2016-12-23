c41c321cfd490e1637bf97fcd5998afccd44fbbe
Travis CI <lars.lubkoll@posteo.de>
2016-12-23 00:35:03 +0000

    Update documentation for commit: daa8b92c0e0d422ce6db11637e99c4e93bae79e8

--- doc/html/search/all_70.js
+++ doc/html/search/all_70.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['p',['P',['../classSpacy_1_1CG_1_1Solver.html#a929d2b9deecfd96ef7efb8097e567f37',1,'Spacy::CG::Solver::P()'],['../classSpacy_1_1CG_1_1LinearSolver.html#aeb4edca3af6d08fcb7700664c2058512',1,'Spacy::CG::LinearSolver::P()']]],
-  ['parameter',['Parameter',['../structSpacy_1_1Newton_1_1Parameter.html',1,'Spacy::Newton']]],
   ['parameter',['Parameter',['../classSpacy_1_1Parameter.html',1,'Spacy']]],
+  ['parameter',['Parameter',['../structSpacy_1_1Newton_1_1Parameter.html',1,'Spacy::Newton']]],
   ['pow',['pow',['../namespaceSpacy.html#acffef7eb409609abe7ad87aaad28503f',1,'Spacy']]],
   ['previous',['previous',['../classSpacy_1_1LipschitzConstant.html#a3a91ae4dea16ff7e83f1d51797145cbd',1,'Spacy::LipschitzConstant']]],
   ['primalcomponent',['primalComponent',['../group__ProductSpaceGroup.html#gaa040ba5c24284687e0df19c99dd688a6',1,'Spacy::primalComponent(::Spacy::Vector &amp;v)'],['../group__ProductSpaceGroup.html#ga88c5bcc74072f75c63ab7d9448f80a7e',1,'Spacy::primalComponent(const ::Spacy::Vector &amp;v)']]],
