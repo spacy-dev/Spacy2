584598a3f3347dd45a33b5692b766c63b5fa2f2c
Lars Lubkoll <lars.lubkoll@posteo.de>
2017-06-03 14:24:03 +0200

    [CMAKE] Fix packaging
    Change-Id: Iefd1484ee18bb1b4cfab26dc85a7468571998c16

--- cmake/SpacyConfig.cmake
+++ cmake/SpacyConfig.cmake
@@ -1,7 +1,7 @@
 # - Config file for the Spacy package 
 set(Spacy_INCLUDE_DIRS ${CMAKE_INSTALL_PREFIX}/include)
-set(Spacy_LIBRARIES ${CMAKE_INSTALL_PREFIX}/lib/libspacy.a)
 set(Spacy_LIBRARY_DIR ${CMAKE_INSTALL_PREFIX}/lib)
+set(Spacy_LIBRARIES ${CMAKE_INSTALL_PREFIX}/lib/libspacy.a)
 set(Spacy_LIBS spacy)
 set(Spacy_CXX_FLAGS "-std=c++1y")
 set(Spacy_CXX_DEFINITIONS "-std=c++1y")
