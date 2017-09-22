810187d43b0c5e0d660bbaa4af06d2ae66b3b47f
Travis CI <lars.lubkoll@posteo.de>
2017-09-22 17:53:52 +0000

    Update documentation for commit: 7b19584a0fbb98f01ac29ed58702a33048eff42c

--- doc/html/search/classes_11.js
+++ doc/html/search/classes_11.js
@@ -6,7 +6,6 @@ var searchData=
   ['traits',['Traits',['../structSpacy_1_1dealII_1_1Traits.html',1,'Spacy::dealII']]],
   ['traits_3c_20variabledims_20_3e',['Traits&lt; VariableDims &gt;',['../structSpacy_1_1dealII_1_1Traits.html',1,'Spacy::dealII']]],
   ['traits_3c_20variabledims_2c_201_20_3e',['Traits&lt; VariableDims, 1 &gt;',['../structSpacy_1_1dealII_1_1Traits_3_01VariableDims_00_011_01_4.html',1,'Spacy::dealII']]],
-  ['transposedlusolver',['TransposedLUSolver',['../classSpacy_1_1FEniCS_1_1TransposedLUSolver.html',1,'Spacy::FEniCS']]],
   ['triangularstateconstraintpreconditioner',['TriangularStateConstraintPreconditioner',['../classSpacy_1_1CG_1_1TriangularStateConstraintPreconditioner.html',1,'Spacy::CG']]],
   ['trustregionsolver',['TrustRegionSolver',['../classSpacy_1_1Algorithm_1_1TrustRegionSolver.html',1,'Spacy::Algorithm']]]
 ];
--- doc/html/search/functions_10.js
+++ doc/html/search/functions_10.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['transposedlusolver',['TransposedLUSolver',['../classSpacy_1_1FEniCS_1_1TransposedLUSolver.html#ae41baf6102474c09945bc03cec8da533',1,'Spacy::FEniCS::TransposedLUSolver']]],
   ['triangularstateconstraintpreconditioner',['TriangularStateConstraintPreconditioner',['../classSpacy_1_1CG_1_1TriangularStateConstraintPreconditioner.html#a8bbfa8bf7bdfcd3c308d4dbec8c448d0',1,'Spacy::CG::TriangularStateConstraintPreconditioner']]],
   ['trustregionsolver',['TrustRegionSolver',['../classSpacy_1_1Algorithm_1_1TrustRegionSolver.html#a0e43b97119b6dc0a25fc9f242b084d16',1,'Spacy::Algorithm::TrustRegionSolver']]]
 ];
