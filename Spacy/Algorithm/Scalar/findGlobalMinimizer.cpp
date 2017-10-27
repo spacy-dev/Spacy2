5af2651e46cb23c92edd5ee788f52209ae4ebdbb
Manuel Schaller <manuelschaller@gmx.de>
2017-10-27 10:00:24 +0200

    added new linesearch technnique
    Change-Id: Id80ec69c975c54413ef879ba1baaff6452d3dfb2

--- Spacy/Algorithm/Scalar/findGlobalMinimizer.cpp
+++ Spacy/Algorithm/Scalar/findGlobalMinimizer.cpp
@@ -1,6 +1,8 @@
 #include "findGlobalMinimizer.hh"
 
 #include <cassert>
+#include <iostream>
+
 
 namespace Spacy
 {
@@ -31,13 +33,45 @@ namespace Spacy
             Real fmin = f(b);
 
             eps = min(eps,0.5);
+            while( (b*=(1-eps)) >= a ) {
+                if (f(b) < fmin) {
+                    fmin = f(b);
+                    tmin = b;
+                }
+            }
+            return tmin;
+        }
 
-            while( (b*=(1-eps)) >= a )
+        Real findLogGlobalMinimizerForDamping(const std::function<Real(const Real)>& f, Real a, Real b, const Real normdn2,
+                                          const Real two_sp_dn_Dt, const Real normDt2, Real eps = 1e-2)
+        {
+            assert(a<b);
+            assert(a>0);
+            Real tmin = b;
+            Real fmin = f(b);
+            eps = min(eps,0.5);
+
+            Real normbefore = 1.e16;
+            Real norm = 0.;
+
+
+            while( (b*=(1-eps)) >= a)
+            {
                 if( f(b) < fmin )
                 {
                     fmin = f(b);
                     tmin = b;
                 }
+                norm = normdn2 + two_sp_dn_Dt*b + b*b*normDt2;
+                // stops the decrease of the damping parameter, if the norm of dn+tmin*Dt increases
+                // This is possible, if dn and Dt are not orthogonal.
+                if(norm > normbefore)
+                {
+                    std::cout<<"norm increased: stopping tangential damping"<<std::endl;
+                    break;
+                }
+                normbefore = norm;
+            }
 
             return tmin;
         }
--- Spacy/Algorithm/Scalar/findGlobalMinimizer.hh
+++ Spacy/Algorithm/Scalar/findGlobalMinimizer.hh
@@ -26,5 +26,21 @@ namespace Spacy
          * @return \f$ x \in \mathrm{argmin}_{[a,b]} \f$
          */
         Real findLogGlobalMinimizer(const std::function<Real(const Real)>& f, Real a, Real b, Real eps = 1e-2);
+
+        /**
+         * @brief Find log.global minimizer of \f$f\f$ in \f$[a,b]\f$ for inexact composite step methods
+         * @param f Nonlinear function \f$ f: \mathbb{R}\to\mathbb{R} \f$
+         * @param a lower bound of the admissible interval
+         * @param b upper bound of the admissible interval
+         * @param normdn2 squared norm of damped normal step
+         * @param two_sp_dn_Dt twice the scalar product of dn and Dt
+         * @param normDt2 squared norm of undamped tangential step
+         * @param eps relative accuracy
+         * @return \f$ x \in \mathrm{argmin}_{[a,b]} \f$ such that it dampens \f$ \|dn+x*Dt\| \f$
+         */
+
+        Real findLogGlobalMinimizerForDamping(const std::function<Real(const Real)>& f, Real a, Real b, const Real normdn2,
+                                          const Real two_sp_dn_Dt, const Real normDt2, Real eps = 1e-2);
+
     }
 }
