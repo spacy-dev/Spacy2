5d15698e578cdd955573d1c36d81e204e691dd8d
Manuel Schaller <manuelschaller@gmx.de>
2017-10-27 10:00:24 +0200

    added new linesearch technnique
    Change-Id: Id80ec69c975c54413ef879ba1baaff6452d3dfb2

--- Spacy/Algorithm/Scalar/findGlobalMinimizer.cpp
+++ Spacy/Algorithm/Scalar/findGlobalMinimizer.cpp
@@ -1,6 +1,7 @@
 #include "findGlobalMinimizer.hh"
 
 #include <cassert>
+#include <iostream>
 
 namespace Spacy
 {
@@ -31,14 +32,47 @@ namespace Spacy
             Real fmin = f(b);
 
             eps = min(eps,0.5);
-
             while( (b*=(1-eps)) >= a )
-                if( f(b) < fmin )
+                if (f(b) < fmin)
                 {
                     fmin = f(b);
                     tmin = b;
                 }
 
+            return tmin;
+        }
+
+        Real findLogGlobalMinimizeTangentialDamping(const std::function<Real(const Real)>& f, Real a, Real b, const Real normdn2,
+                                              const Real two_sp_dn_Dt, const Real normDt2, Real eps)
+        {
+            assert(a<b);
+            assert(a>0);
+            Real tmin = b;
+            Real fmin = f(b);
+            eps = min(eps,0.5);
+
+            Real normbefore = 1.e16;
+            Real norm = 0.;
+
+            while( (b*=(1-eps)) >= a)
+            {
+//                std::cout<<b<<std::endl;
+                if( f(b) < fmin )
+                {
+                    fmin = f(b);
+                    tmin = b;
+                }
+                norm = normdn2 + two_sp_dn_Dt*b + b*b*normDt2;
+
+                // stops the decrease of the damping parameter, if the norm of dn+tmin*Dt increases
+                // This is possible, if dn and Dt are not orthogonal.
+                if(norm > normbefore)
+                {
+                    std::cout<<"norm increased: stopping tangential damping"<<std::endl;
+                    break;
+                }
+                normbefore = norm;
+            }
             return tmin;
         }
     }
--- Spacy/Algorithm/Scalar/findGlobalMinimizer.hh
+++ Spacy/Algorithm/Scalar/findGlobalMinimizer.hh
@@ -18,7 +18,7 @@ namespace Spacy
         Real findGlobalMinimizer(const std::function<Real(const Real)>& f, Real a, Real b, Real eps = 1e-2);
 
         /**
-         * @brief Find log.global minimizer of \f$f\f$ in \f$[a,b]\f$.
+         * @brief Find log global minimizer of \f$f\f$ in \f$[a,b]\f$.
          * @param f Nonlinear function \f$ f: \mathbb{R}\to\mathbb{R} \f$
          * @param a lower bound of the admissible interval
          * @param b upper bound of the admissible interval
@@ -26,5 +26,25 @@ namespace Spacy
          * @return \f$ x \in \mathrm{argmin}_{[a,b]} \f$
          */
         Real findLogGlobalMinimizer(const std::function<Real(const Real)>& f, Real a, Real b, Real eps = 1e-2);
+
+        /**
+         * @brief Find log global minimizer of \f$f\f$ in \f$[a,b]\f$ for inexact composite step methods.
+         * Performs a scalar linesearch on the parameter \f$ x \f$ of \f$ dn+x*Dt \f$ such that this sum minimizes the function \f$ f \f$
+         * It will start from the upper bound and proceed to the lower bound logarithmically, however it will stop, as soon as the
+         * norm of \f$ dn+x*Dt \f$ increases for decreasing \f$ x \f$. This is possible, if dn and Dt are not orthogonal.
+         * @param f Nonlinear function \f$ f: \mathbb{R}\to\mathbb{R} \f$
+         * @param a lower bound of the admissible interval
+         * @param b upper bound of the admissible interval
+         * @param normdn2 squared norm of damped normal step
+         * @param two_sp_dn_Dt twice the scalar product of dn and Dt
+         * @param normDt2 squared norm of undamped tangential step
+         * @param eps relative accuracy
+         * @return \f$ x \in \mathrm{argmin}_{[a,b]} \f$ such that it minimizes \f$ f(dn+x*dt) \f$ without increasing the norm of
+         * the dn+x*dt
+         */
+
+        Real findLogGlobalMinimizerTangentialDamping(const std::function<Real(const Real)>& f, Real a, Real b, const Real normdn2,
+                                              const Real two_sp_dn_Dt, const Real normDt2, Real eps = 1e-2);
+
     }
 }
