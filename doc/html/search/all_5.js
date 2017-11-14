b7fc36ab61e4a77568f74957449469471d25546d
Travis CI <lars.lubkoll@posteo.de>
2017-11-14 11:22:00 +0000

    Update documentation for commit: 545c42c4e3eca7feb8bcd17160d193eb3956f153

--- doc/html/search/all_5.js
+++ doc/html/search/all_5.js
@@ -3,6 +3,7 @@ var searchData=
   ['fenics',['FEniCS',['../group__FenicsGroup.html',1,'']]],
   ['findglobalminimizer',['findGlobalMinimizer',['../namespaceSpacy_1_1Scalar.html#ae3d9bbc5d6c84eafb70737dd8a96251c',1,'Spacy::Scalar']]],
   ['findlogglobalminimizer',['findLogGlobalMinimizer',['../namespaceSpacy_1_1Scalar.html#a4692ea99ec81e957a20415d52659fd42',1,'Spacy::Scalar']]],
+  ['findlogglobalminimizertangentialdamping',['findLogGlobalMinimizerTangentialDamping',['../namespaceSpacy_1_1Scalar.html#aafb39f9f92f5f3186c3c2c9c5ce0eced',1,'Spacy::Scalar']]],
   ['foreach',['ForEach',['../structSpacy_1_1ForEach.html',1,'Spacy']]],
   ['foreach_3c_20variableids_3c_20id_2c_20ids_2e_2e_2e_3e_20_3e',['ForEach&lt; VariableIds&lt; id, ids...&gt; &gt;',['../structSpacy_1_1ForEach_3_01VariableIds_3_01id_00_01ids_8_8_8_4_01_4.html',1,'Spacy']]],
   ['functional',['Functional',['../classSpacy_1_1Functional.html',1,'Spacy']]],
--- doc/html/search/functions_4.js
+++ doc/html/search/functions_4.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['findglobalminimizer',['findGlobalMinimizer',['../namespaceSpacy_1_1Scalar.html#ae3d9bbc5d6c84eafb70737dd8a96251c',1,'Spacy::Scalar']]],
   ['findlogglobalminimizer',['findLogGlobalMinimizer',['../namespaceSpacy_1_1Scalar.html#a4692ea99ec81e957a20415d52659fd42',1,'Spacy::Scalar']]],
+  ['findlogglobalminimizertangentialdamping',['findLogGlobalMinimizerTangentialDamping',['../namespaceSpacy_1_1Scalar.html#aafb39f9f92f5f3186c3c2c9c5ce0eced',1,'Spacy::Scalar']]],
   ['functionalbase',['FunctionalBase',['../classSpacy_1_1FunctionalBase.html#aa655b0f2b96f02a3137dad89e3f0d2ac',1,'Spacy::FunctionalBase']]]
 ];
